6632a234ab2709aa767cedafc33f7b87b0015b7c
Diarmuid Ryan <diarmuidr3d@gmail.com>
2023-11-30 19:16:16 +0000

    Add validations to agency and route

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -5,5 +5,7 @@
 class Agency < ApplicationRecord
   extend T::Sig
 
+  validates :gtfs_agency_id, :agency_name, :agency_url, :agency_timezone, presence: true
+
   has_many :routes, inverse_of: :agency
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -11,7 +11,7 @@ class Route < ApplicationRecord
   enum continuous_drop_off: %i[continuous_drop_off_normal continuous_drop_off_not_available
                                continuous_drop_off_must_phone continuous_drop_off_must_coordinate]
 
-  validates :gtfs_route_id, :route_type, :gtfs_agency_id, presence: true
+  validates :gtfs_route_id, :route_type, :gtfs_agency_id, :agency, presence: true
   validates :route_short_name, presence: true, if: proc { |route| route.route_long_name.blank? }
   validates :route_long_name, presence: true, if: proc { |route| route.route_short_name.blank? }
   validates :route_short_name, :route_long_name, length: { maximum: 255 }
@@ -22,7 +22,7 @@ class Route < ApplicationRecord
   validates :route_sort_order, numericality: { only_integer: true }, allow_nil: true
 
   has_many :trips, inverse_of: :route
-  belongs_to :agency, inverse_of: :routes
+  belongs_to :agency
 
   sig { returns(T::Boolean) }
   def any_continuous?
