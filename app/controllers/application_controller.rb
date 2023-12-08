6565f655b1124b4422e2faefd3519189c57e7b31
Diarmuid Ryan <diarmuidr3d@gmail.com>
2023-12-08 18:20:41 +0000

    Add app_id as a required parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 # typed: strict
 
+# This is the parent class of all other controllers
+# Any controller should generally call the load_app method
 class ApplicationController < ActionController::Base
   extend ::T::Sig
 
   sig { returns(App) }
   def load_app
+    params.require(:app_id)
     @app = T.let(nil, T.nilable(App))
     @app = App.find(params[:app_id])
   end
