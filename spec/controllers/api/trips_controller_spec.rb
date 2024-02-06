28bb745b1b1e0bfe0abb5d6d8479e345cc3f4413
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-06 20:34:08 +0000

    Add trips controller spec

--- spec/controllers/api/trips_controller_spec.rb
+++ spec/controllers/api/trips_controller_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe Api::TripsController, type: :controller do
     let(:subject) do
       get :index, params:
     end
+    let(:user) { create(:user, app:) }
+
+    before do
+      login_user(user:)
+    end
 
     context 'with no trips' do
       it 'should return no trips' do
@@ -23,6 +28,13 @@ RSpec.describe Api::TripsController, type: :controller do
         subject
         expect(response).to have_http_status(:success)
         expect(response.body).to eq([trip.serialize].to_json)
+      end
+
+      it 'should not return a trip for a different app' do
+        other_app = create(:app)
+        params[:app_id] = other_app.id
+        subject
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
