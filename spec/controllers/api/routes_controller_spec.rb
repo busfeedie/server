5d22d95241670ddfecc2636a1f09dd295261ba58
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-06 20:26:26 +0000

    update spec for routes controller

--- spec/controllers/api/routes_controller_spec.rb
+++ spec/controllers/api/routes_controller_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe Api::RoutesController, type: :controller do
     let(:subject) do
       get :index, params:
     end
+    let(:user) { create(:user, app:) }
+
+    before do
+      login_user(user:)
+    end
 
     context 'with no routes' do
       it 'should return no routes' do
@@ -23,6 +28,13 @@ RSpec.describe Api::RoutesController, type: :controller do
         subject
         expect(response).to have_http_status(:success)
         expect(response.body).to eq([route.serialize].to_json)
+      end
+
+      it 'should not return a route for a different app' do
+        other_app = create(:app)
+        params[:app_id] = other_app.id
+        subject
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
