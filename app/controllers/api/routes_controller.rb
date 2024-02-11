ed24b27d8ad09f9e8b046050e9c10af8e2729b6f
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-11 17:16:37 +0000

    Increase devise expiry and fix route app access

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -5,12 +5,11 @@ module Api
   # This controller is used to list the available trips
   class RoutesController < ApiController
     extend ::T::Sig
-    protect_from_forgery with: :null_session
     before_action :load_app
 
     sig { returns(String) }
     def index
-      render json: ::Route.where(app: params[:app_id]).map(&:serialize)
+      render json: ::Route.where(app: @app).map(&:serialize)
     end
   end
 end
