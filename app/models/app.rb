ea8769ccbe34d2a315d7a318a04dbbd401ceca5a
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-14 00:44:27 +0000

    Stop destroys dependencies when it's destroyed

--- app/models/app.rb
+++ app/models/app.rb
@@ -4,16 +4,16 @@
 # This represents a particular public transport company and references the objects that belong to it.
 class App < ActiveRecord::Base
   extend T::Sig
-  has_many :agencies, inverse_of: :app
-  has_many :calendar_dates, inverse_of: :app
-  has_many :calendars, inverse_of: :app
-  has_many :routes, inverse_of: :app
-  has_many :shapes, inverse_of: :app
-  has_many :stop_times, inverse_of: :app
-  has_many :stops, inverse_of: :app
-  has_many :trip_identifiers, inverse_of: :app
-  has_many :trips, inverse_of: :app
-  has_many :users, inverse_of: :app
-  has_many :vehicle_positions, inverse_of: :app
-  has_many :vehicles, inverse_of: :app
+  has_many :agencies, inverse_of: :app, dependent: :destroy
+  has_many :calendar_dates, inverse_of: :app, dependent: :destroy
+  has_many :calendars, inverse_of: :app, dependent: :destroy
+  has_many :routes, inverse_of: :app, dependent: :destroy
+  has_many :shapes, inverse_of: :app, dependent: :destroy
+  has_many :stop_times, inverse_of: :app, dependent: :destroy
+  has_many :stops, inverse_of: :app, dependent: :destroy
+  has_many :trip_identifiers, inverse_of: :app, dependent: :destroy
+  has_many :trips, inverse_of: :app, dependent: :destroy
+  has_many :users, inverse_of: :app, dependent: :destroy
+  has_many :vehicle_positions, inverse_of: :app, dependent: :destroy
+  has_many :vehicles, inverse_of: :app, dependent: :destroy
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -8,8 +8,9 @@ class Stop < ApplicationRecord
 
   enum location_type: %i[stop station entrance generic_node boarding_area]
   enum wheelchair_boarding: %i[unknown accessible not_accessible]
-  has_many :child_stops, class_name: 'Stop', foreign_key: 'parent_station_id'
+  has_many :child_stops, class_name: 'Stop', foreign_key: 'parent_station_id', dependent: :destroy
   belongs_to :parent_station, class_name: 'Stop', optional: true
+  has_many :stop_times, inverse_of: :stop, dependent: :destroy
 
   validates :gtfs_stop_id, presence: true
   validates :stop_name, :lonlat, presence: true, if: proc { |stop|
