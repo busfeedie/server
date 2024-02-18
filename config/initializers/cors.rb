e8c12329fab410396243c9abf5ef510f39b604a6
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-18 21:30:54 +0000

    Add additional CORS routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,22 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: %i[get patch put delete post options show]
     )
   end
+  allow do
+    origins 'https://web.busfeed.ie'
+    resource(
+      '*',
+      headers: :any,
+      expose: %w[access-token expiry token-type Authorization],
+      methods: %i[get patch put delete post options show]
+    )
+  end
+  allow do
+    origins 'https://api.busfeed.ie'
+    resource(
+      '*',
+      headers: :any,
+      expose: %w[access-token expiry token-type Authorization],
+      methods: %i[get patch put delete post options show]
+    )
+  end
 end
