9ea327efe664e5d7445ae444c2eff975123961bf
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-19 21:55:59 +0000

    allow CORS for localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,24 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-    resource(
-      '*',
-      headers: :any,
-      expose: %w[access-token expiry token-type Authorization],
-      methods: %i[get patch put delete post options show]
-    )
-  end
-  allow do
-    origins 'https://web.busfeed.ie'
-    resource(
-      '*',
-      headers: :any,
-      expose: %w[access-token expiry token-type Authorization],
-      methods: %i[get patch put delete post options show]
-    )
-  end
-  allow do
     origins 'https://api.busfeed.ie'
+    resource(
+      '*',
+      headers: :any,
+      expose: %w[access-token expiry token-type Authorization],
+      methods: %i[get patch put delete post options show]
+    )
+  end
+  allow do
+    origins 'http://localhost:3000', 'https://web.busfeed.ie', 'http://localhost:52609'
     resource(
       '*',
       headers: :any,
