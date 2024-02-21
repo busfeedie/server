e5fbd46391591bc436bd471806bc3e4fb6e0734d
Diarmuid Ryan <diarmuidr3d@gmail.com>
2024-02-21 21:53:10 +0000

    Allow any localhost port

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://api.busfeed.ie'
-    resource(
-      '*',
-      headers: :any,
-      expose: %w[access-token expiry token-type Authorization],
-      methods: %i[get patch put delete post options show]
-    )
-  end
-  allow do
-    origins 'http://localhost:3000', 'https://web.busfeed.ie', 'http://localhost:52609'
+    origins 'https://api.busfeed.ie', 'https://web.busfeed.ie', %r{\Ahttp://localhost:[0-9]{1,5}\z}
     resource(
       '*',
       headers: :any,
